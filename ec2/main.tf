381e6e0e483afef6666d7b63a85af67cb96531f0
pvarughese23 <pvarughese@luc.edu>
2024-06-04 08:17:35 -0500

    add ssm patch

--- ec2/main.tf
+++ ec2/main.tf
@@ -6,14 +6,6 @@ resource "aws_instance" "example_server" {
     PatchGroup  = aws_ssm_patch_group.al2_patch_group.patch_group
   }
 }
-resource "aws_instance" "example_server" {
-  ami           = var.ami_id
-  instance_type = var.web_instance_type
-  tags = {
-    Name        = "MyAmazonLinux2023Instance"
-    PatchGroup  = aws_ssm_patch_group.al2_patch_group.patch_group
-  }
-}
 
 resource "aws_ssm_patch_baseline" "al2_patch_baseline" {
   name             = "AmazonLinux2PatchBaseline"
